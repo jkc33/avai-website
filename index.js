a448d613eb756987b968684616a069d5174fb180
Jiakang <jiakang@pop-os.localdomain>
2021-09-05 00:14:34 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,237 @@
+let competitions = [
+  {
+    name: 'Optiver Realized',
+    date: '09/02/2021',
+    image: 'images/comp-1.svg',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    kaggle: 'https://www.kaggle.com/',
+    slides: 'https://docs.google.com/presentation/u/0/',
+    colab: 'https://colab.research.google.com/',
+    code: 'https://www.python.org/',
+    prize1: 'x2 Candy',
+    prize2: 'x1 Candy',
+    prize3: 'x1 Candy',
+  },
+  {
+    name: 'NFL Health/Safety',
+    date: '08/25/2021',
+    image: 'images/comp-2.svg',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    kaggle: 'https://www.kaggle.com/',
+    slides: 'https://docs.google.com/presentation/u/0/',
+    colab: 'https://colab.research.google.com/',
+    code: 'https://www.python.org/',
+    prize1: 'x2 Candy',
+    prize2: 'x1 Candy',
+    prize3: 'x1 Candy',
+  },
+  {
+    name: 'RSNA-MICCAI',
+    date: '08/18/2021',
+    image: 'images/comp-3.svg',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    kaggle: 'https://www.kaggle.com/',
+    slides: 'https://docs.google.com/presentation/u/0/',
+    colab: 'https://colab.research.google.com/',
+    code: 'https://www.python.org/',
+    prize1: 'x2 Candy',
+    prize2: 'x1 Candy',
+    prize3: 'x1 Candy',
+  },
+  {
+    name: 'LearnPlatform',
+    date: '08/11/2021',
+    image: 'images/comp-4.svg',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    kaggle: 'https://www.kaggle.com/',
+    slides: 'https://docs.google.com/presentation/u/0/',
+    colab: 'https://colab.research.google.com/',
+    code: 'https://www.python.org/',
+    prize1: 'x2 Candy',
+    prize2: 'x1 Candy',
+    prize3: 'x1 Candy',
+  },
+  {
+    name: 'G2Net Gravitational',
+    date: '08/04/2021',
+    image: 'images/comp-5.svg',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    kaggle: 'https://www.kaggle.com/',
+    slides: 'https://docs.google.com/presentation/u/0/',
+    colab: 'https://colab.research.google.com/',
+    code: 'https://www.python.org/',
+    prize1: 'x2 Candy',
+    prize2: 'x1 Candy',
+    prize3: 'x1 Candy',
+  },
+  {
+    name: 'Lux AI',
+    date: '07/28/2021',
+    image: 'images/comp-6.svg',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    kaggle: 'https://www.kaggle.com/',
+    slides: 'https://docs.google.com/presentation/u/0/',
+    colab: 'https://colab.research.google.com/',
+    code: 'https://www.python.org/',
+    prize1: 'x2 Candy',
+    prize2: 'x1 Candy',
+    prize3: 'x1 Candy',
+  },
+  {
+    name: 'Google Landmark',
+    date: '07/21/2021',
+    image: 'images/comp-7.svg',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    kaggle: 'https://www.kaggle.com/',
+    slides: 'https://docs.google.com/presentation/u/0/',
+    colab: 'https://colab.research.google.com/',
+    code: 'https://www.python.org/',
+    prize1: 'x2 Candy',
+    prize2: 'x1 Candy',
+    prize3: 'x1 Candy',
+  },
+];
+
+const compList = document.getElementById('competition-list');
+const searchBar = document.getElementById('search-bar');
+
+const displayComps = (comps) => {
+  const htmlString = comps
+    .map((comp) => {
+      let week2 = new Date(Date.parse(comp.date) + 2 * 604800000);
+      let week1 = new Date(Date.parse(comp.date) + 604800000);
+      let week0 = new Date(Date.parse(comp.date));
+      return `
+        <li class="transition2" data-modal-target=${'#' + comp.name}>
+          <img class="stagger2" src=${comp.image}> </img>
+          <p class="stagger2" >${comp.date}</p>
+          <h1 class="stagger2">${comp.name}</h1>
+        </li>
+        <div class="modal" id=${comp.name}>
+          <div class="modal-container">
+            <div class="modal-left">
+              <div class="header">
+                <p class="modal-date">${comp.date}</p>
+                <h1>${comp.name}</h1>
+              </div>
+              <div class="desc">
+                <p class="modal-desc">${comp.desc}</p>
+              </div>
+              <div class="left-links">
+                <div class="left-card">
+                  <a href=${comp.slides} target="_blank">
+                    <img src="images/slides.svg"> </img>
+                  </a>
+                  <div class="caption">
+                    <h1>Topic Slides</h1>
+                    <p>${(week0.getMonth() + 1).toString()}/${week0
+        .getDate()
+        .toString()}</p>
+                  </div>
+                </div>
+                <div class="left-card">
+                  <a href=${comp.colab} target="_blank">
+                    <img src="images/colab.svg"> </img>
+                  </a>
+                  <div class="caption">
+                    <h1>Scaffolding</h1>
+                    <p>${(week1.getMonth() + 1).toString()}/${week1
+        .getDate()
+        .toString()}</p>
+                  </div>
+                </div>
+                <div class="left-card">
+                  <a href=${comp.code} target="_blank">
+                    <img src="images/python.svg"> </img>
+                  </a>
+                  <div class="caption">
+                    <h1>Example Code</h1>
+                    <p>${(week2.getMonth() + 1).toString()}/${week2
+        .getDate()
+        .toString()}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="modal-right">
+              <img data-close-button class="close-button" src="images/exit.svg"> </img>
+              <div class="right-card">
+                <a href=${comp.kaggle} target="_blank">
+                  <img class="kaggle" src="images/kaggle.svg"> </img>
+                </a>
+              </div>
+              <div class="bottom-card">
+                <div class="title">
+                  <h1> Prizes </h1>
+                  <img src="images/trophy.svg" </img>
+                </div>
+                <div class="prizes">
+                  <p>
+                    <strong> 1st: </strong> &nbsp;&nbsp;&nbsp;${comp.prize1}
+                  </p>
+                  <p>
+                    <strong> 2nd: </strong> &nbsp;&nbsp;&nbsp;${comp.prize2}
+                  </p>
+                  <p>
+                    <strong> 3rd: </strong> &nbsp;&nbsp;&nbsp;${comp.prize3}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+    })
+    .join('');
+  compList.innerHTML = htmlString;
+  const openModalButtons = document.querySelectorAll('[data-modal-target]');
+  const overlay = document.getElementById('overlay');
+  const closeModalButtons = document.querySelectorAll('[data-close-button]');
+
+  openModalButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget);
+      openModal(modal);
+    });
+  });
+
+  overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach((modal) => {
+      closeModal(modal);
+    });
+  });
+
+  closeModalButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal');
+      closeModal(modal);
+    });
+  });
+};
+
+displayComps(competitions);
+
+searchBar.addEventListener('keyup', (e) => {
+  const searchString = e.target.value.toLowerCase();
+
+  const filtered = competitions.filter((comp) => {
+    return (
+      comp.name.toLowerCase().includes(searchString) ||
+      comp.date.toLowerCase().includes(searchString)
+    );
+  });
+  displayComps(filtered);
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
